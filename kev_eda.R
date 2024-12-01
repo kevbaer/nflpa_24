5a47512a44b9d287aa0b530e38c2e6bca8d89425
Kevin Baer <149030800+kevbaer@users.noreply.github.com>
2024-11-30 23:18:09 -0800

    Yeah this was a lot

--- kev_eda.R
+++ kev_eda.R
@@ -1,12 +1,14 @@
 # Mailroom ----------------------------------------------------------------
+set.seed(11042004)
 
 library(nflreadr)
 library(tidyclust)
 library(tidyverse)
 library(tidymodels)
-set.seed(11042004)
 library(wesanderson)
 library(gt)
+library(ggthemes)
+library(patchwork)
 # Data stuff --------------------------------------------------------------
 
 verse <- load_players()
@@ -58,14 +60,14 @@ df_added |>
 
 # setup for kmeans --------------------------------------------------------
 kmeans_df <- df |> 
-  drop_na(c(guaranteed, apy)) |> 
-  select(gsis_id, player,year_signed, apy, guaranteed, is_active)
+  drop_na(c(guaranteed, apy_cap_pct)) |> 
+  select(gsis_id, player,year_signed, apy, guaranteed, is_active, inflated_apy, inflated_guaranteed)
 
 
 
 # shuffle rows
 kmeans_df <- kmeans_df |> 
-  slice_sample(n = nrow(kmeans_df))
+  arrange(inflated_apy)
 # WSS ---------------------------------------------------------------------
 
 n <- 10
@@ -76,7 +78,7 @@ for (i in 1:n) {
   kmeans_spec <- k_means(num_clusters = i)
   
   kmeans_fit <- kmeans_spec |> 
-    fit(~ apy + guaranteed,
+    fit(~ inflated_apy + inflated_guaranteed,
         data = kmeans_df
     )
   kmeans_summary <- kmeans_fit |>
@@ -106,17 +108,19 @@ scree_plot
 kmeans_spec <- k_means(num_clusters = 4)
 
 kmeans_fit <- kmeans_spec |> 
-  fit(~ apy + guaranteed,
+  fit(~ inflated_apy + inflated_guaranteed,
       data = kmeans_df
   )
 
 
 final <- kmeans_fit |> 
-  augment(kmeans_df)
+  augment(kmeans_df) |> 
+  mutate(added = inflated_apy + inflated_guaranteed)|> 
+  filter(!(added > 15 & inflated_guaranteed == 0))
 
 final |> 
-  ggplot(aes(apy, guaranteed, colour = .pred_cluster, shape = .pred_cluster)) +
-  geom_point() 
+  ggplot(aes(inflated_apy, inflated_guaranteed, colour = .pred_cluster, shape = .pred_cluster)) +
+  geom_point(alpha = 0.7) 
 
 # plot ------------------------------------------------------------
 
@@ -128,10 +132,10 @@ working <- final |>
       year_signed %in% c(2015,2016,2017) ~ "Past 2015-2017",
       year_signed %in% c(2022,2023,2024) ~ "Current 2022-2024",
     )
-  ) |> 
-  mutate(added = guaranteed + apy)
-
-
+  ) |>   # These players were briefly on the franchise tag but guaranteed money is wrong
+  arrange(-inflated_apy) |> 
+  distinct(gsis_id, Time, .keep_all=TRUE)
+  
 working |> 
   ggplot(aes(fill = Time, x = .pred_cluster, y = after_stat(prop*100), 
              group = fct_rev(Time), 
@@ -141,44 +145,43 @@ working |>
             position = position_dodge(.9), 
             vjust = -0.5, 
             size = 3) +
-  ylab("Percentage of players in era per cluster") +
+  ylab("Percentage of players") +
   xlab("Clusters")+
   theme(
-    legend.position = c(.95, .95),
-    legend.justification = c("right", "top"),
-    legend.box.just = "right",
-    legend.margin = margin(6, 6, 6, 6))+
-  scale_fill_manual(values=wesanderson::wes_palette(n=2, name="GrandBudapest1"), drop = FALSE)
-
+    legend.position = "inside", legend.position.inside = c(0.9, 0.8))+
+  scale_fill_manual(values=wesanderson::wes_palette(n=2, name="GrandBudapest1"), drop = FALSE,  
+                    guide = guide_legend(reverse = TRUE))+
+  theme_clean()
 
 era1players <- working |> 
   filter(Time == "Past 2015-2017")
 
 presentplayers <- working |> 
-  filter(Time == "Current 2022-2024")
+  filter(Time == "Current 2022-2024") 
 
 cluster1players <- working |> 
-  filter(.pred_cluster == "Cluster_1") |> 
-  arrange(-added) |> 
-  filter(!row_number() %in% 1:4) # These players were briefly on the franchise tag but guaranteed money is wrong
-
+  filter(.pred_cluster == "Cluster_1") 
+  
+  
 cluster2players <- working |> 
-  filter(.pred_cluster == "Cluster_2")
+  filter(.pred_cluster == "Cluster_2") 
 
 cluster3players <- working |> 
-  filter(.pred_cluster == "Cluster_3")
+  filter(.pred_cluster == "Cluster_3") 
 
 cluster4players <- working |> 
-  filter(.pred_cluster == "Cluster_4")
+  filter(.pred_cluster == "Cluster_4") 
 
 
 pal <- wes_palette(2, name = "GrandBudapest1")
 pal <- unclass(pal)
 
-cluster4players |> 
-  select(year_signed, player, apy, guaranteed) |>
-  slice_sample(n = nrow(cluster4players)) |> 
-  mutate(apy = round(apy, 2),guaranteed= round(guaranteed, 2)) |> 
+set.seed(11042004)
+
+c4 <- cluster4players |> 
+  select(year_signed, player, inflated_apy, inflated_guaranteed) |>
+  slice_sample(n = 10) |> 
+  mutate(inflated_apy = round(inflated_apy, 2),inflated_guaranteed= round(inflated_guaranteed, 2)) |> 
   gt() |> 
   tab_style(
     style = cell_fill(color = pal[1]),
@@ -191,6 +194,78 @@ cluster4players |>
     locations = cells_body(
       columns = year_signed,
       rows = year_signed < 2022
-    ))
+    ))|> 
+  tab_header(
+    title = md("Sample of **Cluster 4**"),
+    subtitle = md("random selection of 10")
+  )
+
+
+c3 <- cluster3players |> 
+  select(year_signed, player, inflated_apy, inflated_guaranteed) |>
+  slice_sample(n = 10) |> 
+  mutate(inflated_apy = round(inflated_apy, 2),inflated_guaranteed= round(inflated_guaranteed, 2)) |> 
+  gt() |> 
+  tab_style(
+    style = cell_fill(color = pal[1]),
+    locations = cells_body(
+      columns = year_signed,
+      rows = year_signed >= 2022
+    )) |> 
+  tab_style(
+    style = cell_fill(color = pal[2]),
+    locations = cells_body(
+      columns = year_signed,
+      rows = year_signed < 2022
+    ))|> 
+  tab_header(
+    title = md("Sample of **Cluster 3**"),
+    subtitle = md("random selection of 10")
+  )
+
+
+c2 <- cluster2players |> 
+  select(year_signed, player, inflated_apy, inflated_guaranteed) |>
+  slice_sample(n = 10) |> 
+  mutate(inflated_apy = round(inflated_apy, 2),inflated_guaranteed= round(inflated_guaranteed, 2)) |> 
+  gt() |> 
+  tab_style(
+    style = cell_fill(color = pal[1]),
+    locations = cells_body(
+      columns = year_signed,
+      rows = year_signed >= 2022
+    )) |> 
+  tab_style(
+    style = cell_fill(color = pal[2]),
+    locations = cells_body(
+      columns = year_signed,
+      rows = year_signed < 2022
+    )) |> 
+  tab_header(
+    title = md("Sample of **Cluster 2**"),
+    subtitle = md("random selection of 10")
+  )
+
+c1 <- cluster1players |> 
+  select(year_signed, player, inflated_apy, inflated_guaranteed) |>
+  slice_sample(n = 10) |> 
+  mutate(inflated_apy = round(inflated_apy, 2),inflated_guaranteed= round(inflated_guaranteed, 2)) |> 
+  gt() |> 
+  tab_style(
+    style = cell_fill(color = pal[1]),
+    locations = cells_body(
+      columns = year_signed,
+      rows = year_signed >= 2022
+    )) |> 
+  tab_style(
+    style = cell_fill(color = pal[2]),
+    locations = cells_body(
+      columns = year_signed,
+      rows = year_signed < 2022
+    )) |> 
+  tab_header(
+    title = md("Sample of **Cluster 1**"),
+    subtitle = md("random selection of 10")
+  )
 
 
